75d01eac35e75f1cd4ce0852e7ded761a0d6cef9
Yun Jang <jang.yunj7@gmail.com>
2023-04-08 01:37:22 -0400

    third commit

--- MyStore/Pages/Clients/Create.cshtml.cs
+++ MyStore/Pages/Clients/Create.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyStore.Pages.NewFolder2;
 
 namespace MyStore.Pages.Clients
 {
     public class CreateModel : PageModel
     {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public String successMessage = "";
         public void OnGet()
         {
+        }
+
+        public void OnPost()
+        {
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                errorMessage = "All the fields are required";
+                return;
+            }
+
+            //save the client into the database
+
+            clientInfo.name = "";
+            clientInfo.name = "";
         }
     }
 }
